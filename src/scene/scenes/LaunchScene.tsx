79d0e11e4587d1115baa6339bacd73347f1ed86b
ajd6735 <117790810+ajd6735@users.noreply.github.com>
2023-11-03 10:11:17 -0400

    Husky pre-commit fails with code 1 (error)

--- src/scene/scenes/LaunchScene.tsx
+++ src/scene/scenes/LaunchScene.tsx
@@ -1,9 +1,11 @@
 import { FalconHeavy } from '../../artifacts/FalconHeavy.tsx';
 import { SceneComponent } from '../types/scene-component.ts';
 import { LaunchPad } from '../../artifacts/LaunchPad.tsx';
+import { LaunchDate } from '../../artifacts/LaunchDate.tsx';
 
 const padScale = 0.5;
 const falconScale = 0.3;
+const dateScale = 0.07;
 
 /**
  * The launch scene which depicts the Psyche mission launch.
@@ -14,6 +16,10 @@ const LaunchScene: SceneComponent = () => (
     <FalconHeavy
       position={[0.75, 1, 0]}
       scale={[falconScale, falconScale, falconScale]}
+    />
+    <LaunchDate
+      position={[1, 5, 1]}
+      scale={[dateScale, dateScale, dateScale]}
     />
   </>
 );
