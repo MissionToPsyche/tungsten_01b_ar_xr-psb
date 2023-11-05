d40c75f228998feb9234894f397cfd5956c3f89f
David Casey <jacob.casey@dev-jtech.com>
2023-11-05 08:51:41 -0800

    adds docs to light bulb

--- src/common/components/LightBulb.tsx
+++ src/common/components/LightBulb.tsx
@@ -3,6 +3,13 @@ import { Sphere } from '@react-three/drei';
 import { ColorRepresentation, MeshStandardMaterial } from 'three';
 import filledVector from '../utils/filled-vector.ts';
 
+/**
+ * A component that acts as a light bulb with both a point light and an emissive sphere to represent the bulb
+ * @param color The color of the light and bulb sphere
+ * @param intensity The intensity of the point light
+ * @param radius The radius of the bulb sphere
+ * @param props Props for the group that wraps the bulb and light
+ */
 const LightBulb = ({
   color,
   intensity,
