af11b691411c07224a8f1ddcba2253fbfc77e08c
David Casey <jacob.casey@dev-jtech.com>
2023-11-18 08:57:45 -0800

    fixes multiple threejs imports issue (#16)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      three: path.resolve('./node_modules/three/build/three.module.js')
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
