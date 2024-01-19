66bb4c0f8095885cc3d267d3f70d0b606817bb48
Avinaash Ghansam <avinaashghansam@avinaashs-mbp.mynetworksettings.com>
2024-01-18 19:42:27 -0500

    add a link to error page so user can click and it will navigate them to the ts guide

--- src/view/ErrorFallBack.tsx
+++ src/view/ErrorFallBack.tsx
@@ -1,15 +1,6 @@
-import {
-  ChakraProvider,
-  Text,
-  Button,
-  Flex,
-  extendTheme
-} from '@chakra-ui/react';
-import { RepeatIcon } from '@chakra-ui/icons';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { ChakraProvider, Flex, Text, extendTheme } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
-// Extend Chakra UI theme here
 const customTheme = extendTheme({
   styles: {
     global: {
@@ -33,41 +24,28 @@ const customTheme = extendTheme({
   }
 });
 
-interface ErrorFallbackProps {
-  error: Error;
-}
-
-function ErrorFallback({ error }: ErrorFallbackProps) {
-  const [pageLoaded, setPageLoaded] = useState(true);
-  const navigate = useNavigate();
-
-  function handleTryAgain() {
-    // Test if the page is loaded
-    setPageLoaded(!pageLoaded);
-  }
-
+function ErrorFallback() {
   return (
     <ChakraProvider theme={customTheme}>
       <div role="alert">
         <Text>Something Went Wrong</Text>
-        <Text>{error.message}</Text>
         <Flex
           justifyContent="center"
           alignItems="center"
           flexDirection="column"
+          mt={4}
         >
-          <Button leftIcon={<RepeatIcon />} onClick={handleTryAgain}>
-            Try again
-          </Button>
-          {!pageLoaded && (
-            <Button
-              onClick={() => {
-                navigate('troubleshooting');
-              }}
-            >
-              Troubleshooting Guide
-            </Button>
-          )}
+          <Link
+            to="/troubleshooting"
+            style={{
+              backgroundColor: 'transparent',
+              color: 'blue',
+              textDecoration: 'underline',
+              fontWeight: 'bold'
+            }}
+          >
+            Troubleshooting Guide
+          </Link>
         </Flex>
       </div>
     </ChakraProvider>
