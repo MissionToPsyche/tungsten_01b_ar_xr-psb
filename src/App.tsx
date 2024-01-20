55cc494be39bf5eebd21802a2e11ceccf44d183b
Avinaash Ghansam <avinaashghansam@avinaashs-mbp.mynetworksettings.com>
2024-01-20 14:13:00 -0500

    remove react router and render the troubleshooting logic in the ErrorFallBack instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-// App.tsx
 import { ChakraProvider } from '@chakra-ui/react';
 import ViewManager from './view/ViewManager.tsx';
 import theme from './common/theme.ts';
 import ModalProvider from './common/modal/ModalProvider.tsx';
 import { AnimationProvider } from './animations/AnimationProvider.tsx';
 import AppErrorBoundaryView from './view/AppErrorBoundaryView.tsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Troubleshooting from './view/TroubleShooting.tsx';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="troubleshooting" element={<Troubleshooting />} />
-        <Route
-          path="*"
-          element={
-            <AppErrorBoundaryView>
-              <ChakraProvider theme={theme} resetCSS={false}>
-                <ModalProvider>
-                  <AnimationProvider>
-                    <ViewManager />
-                  </AnimationProvider>
-                </ModalProvider>
-              </ChakraProvider>
-            </AppErrorBoundaryView>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider theme={theme} resetCSS={false}>
+      <AppErrorBoundaryView>
+        <ModalProvider>
+          <AnimationProvider>
+            <ViewManager />
+          </AnimationProvider>
+        </ModalProvider>
+      </AppErrorBoundaryView>
+    </ChakraProvider>
   );
 }
 export default App;
--- src/view/ErrorFallBack.tsx
+++ src/view/ErrorFallBack.tsx
@@ -1,52 +1,72 @@
-import { ChakraProvider, Flex, Text, extendTheme } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
-
-const customTheme = extendTheme({
-  styles: {
-    global: {
-      body: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh'
-      }
-    }
-  },
-  components: {
-    Text: {
-      baseStyle: {
-        fontWeight: 'bold',
-        fontSize: '2xl',
-        textAlign: 'center'
-      }
-    }
-  }
-});
+import {
+  Heading,
+  Text,
+  UnorderedList,
+  ListItem,
+  Collapse,
+  Box,
+  ChakraProvider
+} from '@chakra-ui/react';
+import { useState } from 'react';
 
 function ErrorFallback() {
+  const [openItem, setOpenItem] = useState<string | null>(null);
+
+  const handleToggle = (item: string) => {
+    setOpenItem(openItem === item ? null : item);
+  };
+
   return (
-    <ChakraProvider theme={customTheme}>
-      <div role="alert">
-        <Text>Something Went Wrong</Text>
-        <Flex
-          justifyContent="center"
-          alignItems="center"
-          flexDirection="column"
-          mt={4}
-        >
-          <Link
-            to="/troubleshooting"
-            style={{
-              backgroundColor: 'transparent',
-              color: 'blue',
-              textDecoration: 'underline',
-              fontWeight: 'bold'
-            }}
-          >
-            Troubleshooting Guide
-          </Link>
-        </Flex>
+    <ChakraProvider>
+      <div>
+        <Heading size="xl">There was an error loading the application</Heading>
+        <Heading size="lg" mt={4} color="magenta.500">
+          Troubleshooting Guide
+        </Heading>
+        <Text>
+          If you&apos;re experiencing issues, follow the steps below to
+          troubleshoot and resolve common problems:
+        </Text>
+        <UnorderedList>
+          <ListItem>
+            <Box
+              as="button"
+              onClick={() => {
+                handleToggle('internetConnection');
+              }}
+              color="blue.500"
+              fontWeight="bold"
+            >
+              Internet Connection
+            </Box>
+            <Collapse in={openItem === 'internetConnection'} animateOpacity>
+              <Box p={4} mt={2} bg="gray.100" rounded="md">
+                Description for Internet Connection troubleshooting...
+              </Box>
+            </Collapse>
+          </ListItem>
+          <ListItem>
+            <Box
+              as="button"
+              onClick={() => {
+                handleToggle('reloadPage');
+              }}
+              color="blue.500"
+              fontWeight="bold"
+            >
+              Reload the page and try again.
+            </Box>
+            <Collapse in={openItem === 'reloadPage'} animateOpacity>
+              <Box p={4} mt={2} bg="gray.100" rounded="md">
+                Description for reloading the page...
+              </Box>
+            </Collapse>
+          </ListItem>
+        </UnorderedList>
+        <Text mt={4}>
+          If the issue persists, please contact our support team for further
+          assistance.
+        </Text>
       </div>
     </ChakraProvider>
   );
