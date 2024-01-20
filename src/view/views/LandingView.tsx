52a0337d8a450c7cd18f520879eb1ba02d6fa280
David Casey <jacob.casey@dev-jtech.com>
2024-01-19 22:07:21 -0800

    improves landing page style

--- src/view/views/LandingView.tsx
+++ src/view/views/LandingView.tsx
@@ -1,5 +1,5 @@
-import { useCallback } from 'react';
-import { Button, Flex, Image, Text } from '@chakra-ui/react';
+import { useCallback, useMemo } from 'react';
+import { Button, Flex, Highlight, Image, Text } from '@chakra-ui/react';
 import { FaRocket } from 'react-icons/fa';
 import { ViewComponent } from '../types/view-component.ts';
 import ViewName from '../types/view-name.ts';
@@ -7,24 +7,32 @@ import { Canvas } from '@react-three/fiber';
 import SceneLighting from '../../common/components/SceneLighting.tsx';
 import LoaderProvider from '../../common/loader/LoaderProvider.tsx';
 import LoaderTracker from '../../common/loader/LoaderTracker.tsx';
-import filledVector from '../../common/utils/filled-vector.ts';
-import { PerspectiveCamera, PresentationControls } from '@react-three/drei';
+import {
+  Bounds,
+  PerspectiveCamera,
+  PresentationControls
+} from '@react-three/drei';
 import { TimeLine } from '../../artifacts/TimeLine.tsx';
-
-const timelineScale = filledVector(0.08);
+import { useWindowSize } from '@uidotdev/usehooks';
 
 /**
  * Landing page for the application, informs the user about the application
  * and allows them to start it when ready.
  */
-
 const LandingView: ViewComponent = ({ changeView }) => {
+  const windowSize = useWindowSize();
+
   const onClickStart = useCallback(() => {
     changeView(ViewName.AR_SCENES);
   }, [changeView]);
 
+  const windowHeight = useMemo(
+    () => windowSize.height ?? window.innerHeight,
+    [windowSize.height]
+  );
+
   return (
-    <Flex height="82vh" flexDir="column" justifyContent="space-between" p={6}>
+    <Flex height={`${windowHeight}px`} flexDir="column" p={6}>
       <Image
         src="/assets/images/psyche-badge-outline-color.svg"
         alt="Psyche Logo"
@@ -32,29 +40,33 @@ const LandingView: ViewComponent = ({ changeView }) => {
       />
       <LoaderProvider>
         <LoaderTracker />
-        <Canvas style={{ height: '50vh' }}>
+        <Canvas style={{ flex: 1 }}>
           <SceneLighting />
           <directionalLight intensity={0.5} position={[8, 10, 40]} />
-          <PerspectiveCamera makeDefault position={[0, 2, 20]} />
-          <PresentationControls
-            global
-            config={{ mass: 2, tension: 500 }}
-            snap={{ mass: 4, tension: 1500 }}
-            rotation={[0, 0, 0]}
-            polar={[-Math.PI / 3, Math.PI / 3]}
-            azimuth={[-Math.PI / 3, Math.PI / 3]}
-          >
-            <TimeLine position={[0, 0, 0]} scale={timelineScale} />
-          </PresentationControls>
+          <PerspectiveCamera makeDefault position={[0, 50, 200]} />
+          <Bounds fit clip observe margin={1}>
+            <PresentationControls
+              global
+              config={{ mass: 2, tension: 500 }}
+              snap={{ mass: 4, tension: 1500 }}
+              rotation={[0, 0, 0]}
+              polar={[-Math.PI / 3, Math.PI / 3]}
+              azimuth={[-Math.PI / 3, Math.PI / 3]}
+            >
+              <TimeLine />
+            </PresentationControls>
+          </Bounds>
         </Canvas>
       </LoaderProvider>
-      <Text fontSize="20" fontWeight="medium" color="#302244">
-        Ready for an interstellar adventure? Tap
-        <Text fontSize="24" as="span">
-          &lsquo;Start Mission Timeline&rsquo;
-        </Text>{' '}
-        to explore NASA&apos;s Psyche Mission in Augmented Reality. We&apos;ll
-        just need your camera permission to blast off!
+      <Text fontSize={18}>
+        <Highlight
+          query={['Start Mission Timeline', 'camera permission']}
+          styles={{ px: '2', py: '1', rounded: 'full', bg: 'magenta.100' }}
+        >
+          Ready for an interstellar adventure? Tap Start Mission Timeline to
+          explore NASA&apos;s Psyche Mission in Augmented Reality. We&apos;ll
+          just need your camera permission to blast off!
+        </Highlight>
       </Text>
       <Button
         size="xl"
