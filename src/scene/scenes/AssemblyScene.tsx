54baaac15738ab275cc9caa333636a37d314a6af
ajd6735 <117790810+ajd6735@users.noreply.github.com>
2024-01-22 23:00:02 -0500

    adds lights to first scene and improves the orbiter model

--- src/scene/scenes/AssemblyScene.tsx
+++ src/scene/scenes/AssemblyScene.tsx
@@ -1,18 +1,18 @@
-import ExplodeTrigger from '../../common/explode/ExplodeTrigger.tsx';
-import { Box } from '@react-three/drei';
 import Explode from '../../common/explode/Explode.tsx';
 import { SceneComponent } from '../types/scene-component.ts';
 import AssembleAnimation from '../../animations/AssembleAnimation.tsx';
 import { Orbiter } from '../../artifacts/Orbiter.tsx';
+import filledVector from '../../common/utils/filled-vector.ts';
+
+const orbiterScale = filledVector(0.5);
 
 const AssemblyScene: SceneComponent = () => (
   <Explode initialExploded={true}>
-    <ExplodeTrigger>
-      <Box position={[0, 1, 0]} scale={[2.3, 2.5, 2.3]}>
-        <meshBasicMaterial color="grey" />
-      </Box>
-    </ExplodeTrigger>
-    <Orbiter />
+    <ambientLight intensity={0.1} position={[0, 20, 0]}/>
+    <spotLight intensity={1} position={[-10, 6, 4]} color={"lightblue"}/>
+    <spotLight intensity={2} position={[10, 10, 4]} color={"white"}/> 
+    
+    <Orbiter position={[0, 2, 0]} scale={orbiterScale}/>
     <AssembleAnimation />
   </Explode>
 );
