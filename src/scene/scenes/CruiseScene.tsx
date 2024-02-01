69cb436fa621848b5cedf322877e2afe317eea0a
ajd6735 <117790810+ajd6735@users.noreply.github.com>
2024-02-01 12:25:25 -0500

    adds scene names and dates to 3rd and 4th scenes

--- src/scene/scenes/CruiseScene.tsx
+++ src/scene/scenes/CruiseScene.tsx
@@ -1,11 +1,16 @@
-import { Text } from '@react-three/drei';
 import { SceneComponent } from '../types/scene-component.ts';
+import { CruiseDate } from '../../artifacts/CruiseDate.tsx';
+import filledVector from '../../common/utils/filled-vector.ts';
+import { CruiseName } from '../../artifacts/CruiseName.tsx';
 
-/**
- * Placeholder component for the cruise scene
- */
+const dateScale = filledVector(0.35);
+const nameScale = filledVector(1.5);
+
 const CruiseScene: SceneComponent = () => (
-  <Text fontSize={0.5}>Cruise Scene Coming Soon!</Text>
+  <>
+  <CruiseDate position={[2, -2, 4]} scale={dateScale}/>
+  <CruiseName position={[-1.5, 10, -1]}  scale={nameScale}/>
+  </>
 );
 
 export default CruiseScene;
