61fc366ab3107c485617b9b51a4010ca141a9625
David Casey <jacob.casey@dev-jtech.com>
2024-02-12 21:48:13 -0800

    adds tooltips

--- src/common/explode/StaticExplodeElement.tsx
+++ src/common/explode/StaticExplodeElement.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { animated, config, useSpring } from '@react-spring/three';
+import { ExplodeElementProps } from './ExplodeElement.tsx';
 import useExplode from './use-explode.ts';
-
-export type ExplodeElementProps = {
-  children: React.ReactNode | ((isExploded: boolean) => React.ReactNode);
-  startPosition: JSX.IntrinsicElements['group']['position'];
-  explodedPosition: JSX.IntrinsicElements['group']['position'];
-  startRotation: JSX.IntrinsicElements['group']['rotation'];
-  explodedRotation: JSX.IntrinsicElements['group']['rotation'];
-  explodeDelay?: number;
-  unExplodeDelay?: number;
-} & Omit<JSX.IntrinsicElements['group'], 'position' | 'children'>;
+import { config, SpringValue, useSpring } from '@react-spring/three';
+import { Euler, Vector3 } from '@react-three/fiber';
 
 /**
- * A wrapper for an element that should
+ * A wrapper for an element that should explode. However, rather than the object moving, the animated position and rotation are passed to the child function.
  * @param children The child elements that should be wrapped
  * @param startPosition The position that the elements should start at
  * @param explodedPosition The position that the elements should end up at when exploded
@@ -23,7 +15,15 @@ export type ExplodeElementProps = {
  * @param unExplodeDelay Optional delay to apply to the element when un-exploding
  * @param props Any additional props provided to the wrapping group
  */
-const ExplodeElement: React.FC<ExplodeElementProps> = ({
+const StaticExplodeElement: React.FC<
+  Omit<ExplodeElementProps, 'children'> & {
+    children: (
+      isExploded: boolean,
+      position: SpringValue<Vector3>,
+      rotation: SpringValue<Euler>
+    ) => React.ReactNode;
+  }
+> = ({
   children,
   startPosition,
   explodedPosition,
@@ -38,21 +38,17 @@ const ExplodeElement: React.FC<ExplodeElementProps> = ({
   const { rotation, position } = useSpring({
     position: isExploded ? explodedPosition : startPosition,
     rotation: isExploded ? explodedRotation : startRotation,
-    config: config.wobbly,
+    config: config.stiff,
     onStart: addExploding,
     onRest: removeExploding,
     delay: isExploded ? explodeDelay : unExplodeDelay
   });
 
   return (
-    <animated.group
-      {...props}
-      position={position as never}
-      rotation={rotation as never}
-    >
-      {typeof children === 'function' ? children(isExploded) : children}
-    </animated.group>
+    <group {...props}>
+      {children(isExploded, position as never, rotation as never)}
+    </group>
   );
 };
 
-export default ExplodeElement;
+export default StaticExplodeElement;
--- src/scene/scenes/AssemblyScene.tsx
+++ src/scene/scenes/AssemblyScene.tsx
@@ -7,7 +7,9 @@ import filledVector from '../../common/utils/filled-vector.ts';
 import { AssembleTestSceneName } from '../../artifacts/AssembleTestSceneName.tsx';
 import { AssembleDate } from '../../artifacts/AssembleDate.tsx';
 import { Box } from '@react-three/drei';
-import { Text } from '@react-three/drei';
+import ARTooltip from '../../common/components/ARTooltip.tsx';
+import StaticExplodeElement from '../../common/explode/StaticExplodeElement.tsx';
+import FactsModalTrigger from '../../facts/FactsModalTrigger.tsx';
 
 const orbiterScale = filledVector(0.75);
 const sceneNameScale = filledVector(0.9);
@@ -15,13 +17,76 @@ const sceneDateScale = filledVector(0.2);
 
 const AssemblyScene: SceneComponent = () => (
   <Explode initialExploded={true}>
-    <Text
-      fontSize={0.8}
-      position={[0, -1.5, -2]}
-      rotation={[-Math.PI / 8, 0, 0]}
+    <StaticExplodeElement
+      startPosition={[0, 2.25, 0]}
+      startRotation={[0, 0, 0]}
+      explodedPosition={[0, 0.5, -1]}
+      explodedRotation={[0, 0, 0]}
     >
-      Touch the spacecraft to put it together
-    </Text>
+      {(isExploded, position) => (
+        <ARTooltip
+          objectPosition={position.get() as never}
+          panelPosition={[0, -1, -1]}
+          text={`Touch the spacecraft to ${
+            isExploded ? 'put it together' : 'take it apart'
+          }`}
+        />
+      )}
+    </StaticExplodeElement>
+    <FactsModalTrigger factName="spectrometer">
+      <StaticExplodeElement
+        startPosition={[-1, 7, 0]}
+        startRotation={[0, 0, 0]}
+        explodedPosition={[-1, 8.5, -1]}
+        explodedRotation={[0, 0, 0]}
+      >
+        {(isExploded, position) =>
+          (isExploded || !position.idle) && (
+            <ARTooltip
+              objectPosition={position.get() as never}
+              panelPosition={[-10, 10, -1]}
+              text="Spectrometer"
+            />
+          )
+        }
+      </StaticExplodeElement>
+    </FactsModalTrigger>
+    <FactsModalTrigger factName="magnetometer">
+      <StaticExplodeElement
+        startPosition={[1, 7, 0]}
+        startRotation={[0, 0, 0]}
+        explodedPosition={[1, 8.5, -1]}
+        explodedRotation={[0, 0, 0]}
+      >
+        {(isExploded, position) =>
+          (isExploded || !position.idle) && (
+            <ARTooltip
+              objectPosition={position.get() as never}
+              panelPosition={[10, 10, -1]}
+              text="Magnetometer"
+            />
+          )
+        }
+      </StaticExplodeElement>
+    </FactsModalTrigger>
+    <FactsModalTrigger factName="multiSpectralImager">
+      <StaticExplodeElement
+        startPosition={[0, 5, -0.5]}
+        startRotation={[0, 0, 0]}
+        explodedPosition={[0, 5.5, -2.5]}
+        explodedRotation={[0, 0, 0]}
+      >
+        {(isExploded, position) =>
+          (isExploded || !position.idle) && (
+            <ARTooltip
+              objectPosition={position.get() as never}
+              panelPosition={[10, 7, -1]}
+              text="Multispectral Imager"
+            />
+          )
+        }
+      </StaticExplodeElement>
+    </FactsModalTrigger>
     <ambientLight intensity={0.1} position={[0, 20, 0]} />
     <spotLight intensity={0.1} position={[-14, 6, 4]} color={'lightblue'} />
     <spotLight intensity={0.1} position={[10, 10, 0]} color={'#70707e'} />
