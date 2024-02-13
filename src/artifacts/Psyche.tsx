92433aaf35381b9523b9d42989eab727859fe4e5
ajd6735 <117790810+ajd6735@users.noreply.github.com>
2024-02-13 15:18:35 -0500

    run format

--- src/artifacts/Psyche.tsx
+++ src/artifacts/Psyche.tsx
@@ -4,31 +4,41 @@ Command: npx gltfjsx@6.2.13 ./public/assets/models/psyche.gltf -tT -o Psyche.tsx
 Files: ./public/assets/models/psyche.gltf [56.82MB] > psyche-transformed.glb [4.92MB] (91%)
 */
 
-import * as THREE from 'three'
-import { useGLTF } from '@react-three/drei'
-import { GLTF } from 'three-stdlib'
+import * as THREE from 'three';
+import { useGLTF } from '@react-three/drei';
+import { GLTF } from 'three-stdlib';
 
 type GLTFResult = GLTF & {
   nodes: {
-    PSYCHE_20170116_DEC: THREE.Mesh
-    PSYCHE_20170116_DEC001: THREE.Mesh
-  }
+    PSYCHE_20170116_DEC: THREE.Mesh;
+    PSYCHE_20170116_DEC001: THREE.Mesh;
+  };
   materials: {
-    ['Material.002']: THREE.MeshStandardMaterial
-    ['Material.001']: THREE.MeshStandardMaterial
-  }
-}
+    ['Material.002']: THREE.MeshStandardMaterial;
+    ['Material.001']: THREE.MeshStandardMaterial;
+  };
+};
 
 export function Psyche(props: JSX.IntrinsicElements['group']) {
-  const { nodes, materials } = useGLTF('/assets/models/psyche-transformed.glb') as GLTFResult
+  const { nodes, materials } = useGLTF(
+    '/assets/models/psyche-transformed.glb'
+  ) as GLTFResult;
   return (
     <group {...props} dispose={null}>
       <group>
-        <mesh geometry={nodes.PSYCHE_20170116_DEC.geometry} material={materials['Material.002']} />
-        <mesh geometry={nodes.PSYCHE_20170116_DEC001.geometry} material={materials['Material.001']} position={[-0.001, 0, -0.01]} scale={[0.999, 0.999, 0.989]} />
+        <mesh
+          geometry={nodes.PSYCHE_20170116_DEC.geometry}
+          material={materials['Material.002']}
+        />
+        <mesh
+          geometry={nodes.PSYCHE_20170116_DEC001.geometry}
+          material={materials['Material.001']}
+          position={[-0.001, 0, -0.01]}
+          scale={[0.999, 0.999, 0.989]}
+        />
       </group>
     </group>
-  )
+  );
 }
 
-useGLTF.preload('/assets/models/psyche-transformed.glb')
+useGLTF.preload('/assets/models/psyche-transformed.glb');
--- src/scene/scenes/OrbitScene.tsx
+++ src/scene/scenes/OrbitScene.tsx
@@ -18,14 +18,18 @@ const OrbitScene: SceneComponent = () => (
     />
     <OrbitName position={[0, 8.5, -8]} scale={nameScale} />
     <ModelSpinner position={[0, 0, 0]} speed={0}>
-    <Psyche position={[-4.5, -6, -3]} scale={psycheScale} rotation={[Math.PI/4, 0, 0]}/>
+      <Psyche
+        position={[-4.5, -6, -3]}
+        scale={psycheScale}
+        rotation={[Math.PI / 4, 0, 0]}
+      />
     </ModelSpinner>
-    <ambientLight intensity={0}/>
-    <hemisphereLight position={[0, -150, -100]} intensity={0.5}/>
-    <spotLight intensity={2} position={[-4.5, 3, 0]} color={'#5F73E9'}/>
-    <spotLight intensity={6} position={[-4.5, 5, -50]} color={'#A15FE9'}/>
-    <spotLight intensity={4} position={[-4.5, 7, 0]} color={'white'}/>
-    <spotLight intensity={1} position={[-2.5, 7, 2]} color={'white'}/>
+    <ambientLight intensity={0} />
+    <hemisphereLight position={[0, -150, -100]} intensity={0.5} />
+    <spotLight intensity={2} position={[-4.5, 3, 0]} color={'#5F73E9'} />
+    <spotLight intensity={6} position={[-4.5, 5, -50]} color={'#A15FE9'} />
+    <spotLight intensity={4} position={[-4.5, 7, 0]} color={'white'} />
+    <spotLight intensity={1} position={[-2.5, 7, 2]} color={'white'} />
   </>
 );
 
