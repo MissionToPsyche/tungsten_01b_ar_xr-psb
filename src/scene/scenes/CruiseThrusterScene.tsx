a14c7bbeeb3dd186155491978f8e92afe843f73e
ajd6735 <117790810+ajd6735@users.noreply.github.com>
2024-02-23 13:12:25 -0500

    extracts the cruise scene lights into one component

--- src/scene/scenes/CruiseThrusterScene.tsx
+++ src/scene/scenes/CruiseThrusterScene.tsx
@@ -4,6 +4,7 @@ import filledVector from '../../common/utils/filled-vector.ts';
 import { CruiseName } from '../../artifacts/CruiseName.tsx';
 import FactsModalTrigger from '../../facts/FactsModalTrigger.tsx';
 import CruiseThrusterAnimation from '../../animations/CruiseThrusterAnimation.tsx';
+import CruiseSceneLights from '../../common/components/CruiseSceneLights.tsx';
 
 const dateScale = filledVector(0.3);
 const nameScale = filledVector(1.2);
@@ -11,13 +12,7 @@ const nameScale = filledVector(1.2);
 const CruiseThrusterScene: SceneComponent = () => (
   <>
     <CruiseThrusterAnimation />
-    <ambientLight intensity={0.5} position={[2, 10, 0]} />
-    <hemisphereLight position={[-20, 60, -150]} intensity={0.1} />
-    <spotLight intensity={0.5} position={[-6, 6, 0]} color={'lightblue'} />
-    <pointLight intensity={4} position={[-8, 3, 4]} color={'#08029d'} />
-    <pointLight intensity={2} position={[-9.5, 30, 10]} color={'#b94204'} />
-    <pointLight intensity={1} position={[-12.5, 15, 0]} color={'blue'} />
-    <spotLight intensity={0.5} position={[10.5, 26, -2]} color={'#441359'} />
+    <CruiseSceneLights />
     <CruiseName position={[-1.5, 10, -1]} scale={nameScale} />
     <FactsModalTrigger factName="cruiseDate">
       <CruiseDate
