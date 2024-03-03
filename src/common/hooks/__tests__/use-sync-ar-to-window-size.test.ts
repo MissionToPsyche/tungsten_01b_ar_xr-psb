96256fc7b0a1380fb163a1808b604a31c4a82935
David Casey <jacob.casey@dev-jtech.com>
2024-03-02 19:25:23 -0800

    fixes scene size issue

--- src/common/hooks/__tests__/use-sync-ar-to-window-size.test.ts
+++ src/common/hooks/__tests__/use-sync-ar-to-window-size.test.ts
@@ -7,6 +7,26 @@ import syncArToWindowSize from '../../utils/sync-ar-to-window-size.ts';
 vi.mock('@react-three/fiber');
 vi.mock('../../utils/sync-ar-to-window-size.ts');
 
+type ObserverCallback = (
+  mutations: { type: string; attributeName?: string }[]
+) => void;
+
+let callbackTrigger: ObserverCallback;
+
+const mutationObserverMock = vi.fn((callback: ObserverCallback) => {
+  return {
+    observe: vi.fn(() => {
+      callbackTrigger = callback;
+    }),
+    disconnect: vi.fn()
+  };
+});
+
+const videoMock = {} as HTMLVideoElement;
+
+vi.stubGlobal('MutationObserver', mutationObserverMock);
+vi.spyOn(document, 'getElementById').mockReturnValue(videoMock);
+
 const three = {
   gl: vi.fn(),
   camera: vi.fn()
@@ -22,6 +42,8 @@ describe('useSyncArToWindowSize', () => {
     vi.mocked(useThree).mockReturnValueOnce(three);
 
     setup();
+    callbackTrigger([{ type: 'childList' }]);
+    callbackTrigger([{ type: 'attributes', attributeName: 'style' }]);
 
     expect(syncArToWindowSize).toHaveBeenCalledWith(three.gl, three.camera);
   });
--- src/common/hooks/use-sync-ar-to-window-size.ts
+++ src/common/hooks/use-sync-ar-to-window-size.ts
@@ -10,8 +10,39 @@ const useSyncArToWindowSize = () => {
   const { gl, camera } = useThree();
 
   useEffect(() => {
-    syncArToWindowSize(gl, camera as PerspectiveCamera);
-  });
+    // An observer that syncs the AR renderer to the window size when the video element changes
+    const videoObserver = new MutationObserver((mutations) => {
+      mutations.forEach(({ type, attributeName }) => {
+        if (type === 'attributes' && attributeName === 'style') {
+          syncArToWindowSize(gl, camera as PerspectiveCamera);
+        }
+      });
+    });
+
+    // An observer that watches for the video element to be added to the DOM so that the video observer can be attached
+    const videoPresenceObserver = new MutationObserver((mutations) => {
+      mutations.forEach(({ type }) => {
+        if (type === 'childList') {
+          const video = document.getElementById('arjs-video');
+
+          if (!video) {
+            return;
+          }
+
+          syncArToWindowSize(gl, camera as PerspectiveCamera);
+          videoObserver.observe(video, { attributes: true });
+          videoPresenceObserver.disconnect();
+        }
+      });
+    });
+
+    videoPresenceObserver.observe(document.body, { childList: true });
+
+    return () => {
+      videoPresenceObserver.disconnect();
+      videoObserver.disconnect();
+    };
+  }, [camera, gl]);
 };
 
 export default useSyncArToWindowSize;
