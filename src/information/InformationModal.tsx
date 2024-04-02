fcf695058f10e9aeab6a38cc706c9be19ba73d19
Avinaash Ghansam <avinaashghansam@avinaashs-mbp.mynetworksettings.com>
2024-04-02 10:00:26 -0400

    add disclaimer

--- src/information/InformationModal.tsx
+++ src/information/InformationModal.tsx
@@ -77,6 +77,25 @@ const InformationModal: React.FC<InformationModalProps> = ({
                 </ListItem>
               </UnorderedList>
             </InformationItem>
+            <InformationItem title="Disclaimer" index={4}>
+              <Text>
+                This work was created in partial fulfillment of Pennsylvania
+                State University Capstone Course "SWENG 480: Software
+                Engineering Design & SWENG 481: Software Engineering Design II".
+                The work is a result of the Psyche Student Collaborations
+                component of NASA’s Psyche Mission{' '}
+                <a href="https://psyche.asu.edu)">https://psyche.asu.edu</a>{' '}
+                “Psyche: A Journey to a Metal World” [Contract number
+                NNM16AA09C] is part of the NASA Discovery Program mission to
+                solar system targets. Trade names and trademarks of ASU and NASA
+                are used in this work for identification only. Their usage does
+                not constitute an official endorsement, either expressed or
+                implied, by Arizona State University or National Aeronautics and
+                Space Administration. The content is solely the responsibility
+                of the authors and does not necessarily represent the official
+                views of ASU or NASA.
+              </Text>
+            </InformationItem>
           </Accordion>
         </ModalBody>
       </ModalContent>
