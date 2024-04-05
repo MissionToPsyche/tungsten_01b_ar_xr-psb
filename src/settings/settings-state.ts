8e0b18939f79b6ad1c005a4cd91b2785d37c0927
azigerelli <apz5146@psu.edu>
2024-04-05 14:36:18 -0400

    enables audio by default

--- src/settings/settings-state.ts
+++ src/settings/settings-state.ts
@@ -5,7 +5,7 @@ const settingsState = atom<Settings>({
   key: 'settingsState',
   default: {
     arEnabled: true,
-    audioEnabled: false,
+    audioEnabled: true,
     tooltipsEnabled: true
   }
 });
