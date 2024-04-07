c7e558ae73da268ea02a9fa2fac332bc053a09a4
Avinaash Ghansam <102967902+AvinaashGhansam@users.noreply.github.com>
2024-04-06 22:47:26 -0400

    redesign settings modal to align button and give a more professional look (#119)
    * redesign settings modal
    
    * fixed UI from pr suggestion comment
    
    ---------
    
    Co-authored-by: Avinaash Ghansam <avinaashghansam@avinaashs-mbp.mynetworksettings.com>
    Co-authored-by: Avinaash Ghansam <avinaashghansam@Avinaashs-MacBook-Pro.local>

--- src/settings/SettingsModal.tsx
+++ src/settings/SettingsModal.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
 import {
+  Flex,
   FormControl,
   FormLabel,
   Modal,
@@ -13,13 +14,11 @@ import {
 } from '@chakra-ui/react';
 import RenderIf from '../common/components/RenderIf.tsx';
 import useSettings from './use-settings.ts';
-
 interface SettingsWindowProps {
   isOpen: boolean;
   onClose: () => void;
   hideArButton?: boolean;
 }
-
 const SettingsModal = ({
   isOpen,
   onClose,
@@ -54,7 +53,6 @@ const SettingsModal = ({
     },
     [setTooltipsEnabled]
   );
-
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
@@ -65,35 +63,45 @@ const SettingsModal = ({
           <VStack>
             <RenderIf shouldRender={!hideArButton}>
               <FormControl display="flex" alignItems="center">
-                <FormLabel htmlFor="ar-toggle" mb="0">
-                  Enable Augmented Reality?
-                </FormLabel>
-                <Switch
-                  id="ar-toggle"
-                  isChecked={arEnabled}
-                  onChange={onChangeArToggle}
-                />
+                <Flex
+                  justifyContent="space-between"
+                  alignItems="center"
+                  w="100%"
+                >
+                  <FormLabel htmlFor="ar-toggle" mb="0">
+                    Enable Augmented Reality
+                  </FormLabel>
+                  <Switch
+                    id="ar-toggle"
+                    isChecked={arEnabled}
+                    onChange={onChangeArToggle}
+                  />
+                </Flex>
               </FormControl>
             </RenderIf>
             <FormControl display="flex" alignItems="center">
-              <FormLabel htmlFor="audio-toggle" mb="0">
-                Enable Audio?
-              </FormLabel>
-              <Switch
-                id="audio-toggle"
-                isChecked={audioEnabled}
-                onChange={onChangeAudioToggle}
-              />
+              <Flex justifyContent="space-between" alignItems="center" w="100%">
+                <FormLabel htmlFor="audio-toggle" mb="0">
+                  Enable Audio
+                </FormLabel>
+                <Switch
+                  id="audio-toggle"
+                  isChecked={audioEnabled}
+                  onChange={onChangeAudioToggle}
+                />
+              </Flex>
             </FormControl>
             <FormControl display="flex" alignItems="center">
-              <FormLabel htmlFor="tooltips-toggle" mb="0">
-                Enable Tooltips?
-              </FormLabel>
-              <Switch
-                id="tooltips-toggle"
-                isChecked={tooltipsEnabled}
-                onChange={onChangeTooltipsToggle}
-              />
+              <Flex justifyContent="space-between" alignItems="center" w="100%">
+                <FormLabel htmlFor="tooltips-toggle" mb="0">
+                  Enable Tooltips
+                </FormLabel>
+                <Switch
+                  id="tooltips-toggle"
+                  isChecked={tooltipsEnabled}
+                  onChange={onChangeTooltipsToggle}
+                />
+              </Flex>
             </FormControl>
           </VStack>
         </ModalBody>
