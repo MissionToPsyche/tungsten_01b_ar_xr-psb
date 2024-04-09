7a148122b676ea8f42cae50cbf3833454af7169e
ajd6735 <117790810+ajd6735@users.noreply.github.com>
2024-04-09 14:06:33 -0400

    adds exploder to the pannels animation

--- src/scene/scenes/CruisePanelsScene.tsx
+++ src/scene/scenes/CruisePanelsScene.tsx
@@ -7,22 +7,39 @@ import ARTooltip from '../../common/components/ARTooltip.tsx';
 import useScene from '../use-scene.ts';
 import RenderIf from '../../common/components/RenderIf.tsx';
 import { Environment } from '@react-three/drei';
+import { Earth } from '../../artifacts/Earth.tsx';
+import { InnitialCheckoutOrbiter } from '../../artifacts/InnitialCheckoutOrbiter.tsx';
+import filledVector from '../../common/utils/filled-vector.ts';
+import Explode from '../../common/explode/Explode.tsx';
+
+const earthScale = filledVector(5);
+const orbiterScale = filledVector(0.75);
 
 const CruisePanelsScene: SceneComponent = () => {
   const { isTransitioning } = useScene();
 
   return (
-    <>
+    <Explode initialExploded={true}>
       <CruisePanelsAnimation />
       <BackAnimation />
       <CruiseSceneLights />
       <Environment preset="forest" />
       <RenderIf shouldRender={!isTransitioning}>
         <FactsModalTrigger factName="solarPanels">
-          <ARTooltip position={[1, 2, 3]} />
+          <ARTooltip position={[-2.5, -1.9, 4]} />
         </FactsModalTrigger>
       </RenderIf>
-    </>
+      <Earth
+        position={[6, -4, -15]}
+        scale={earthScale}
+        rotation={[0, Math.PI / 2, 0]}
+      />
+      <InnitialCheckoutOrbiter
+        position={[0, 0, 2]}
+        scale={orbiterScale}
+        rotation={[Math.PI / 5, Math.PI / 5, Math.PI / 6]}
+      />
+    </Explode>
   );
 };
 
