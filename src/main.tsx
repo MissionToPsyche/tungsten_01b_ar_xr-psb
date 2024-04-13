619ec829fde4cbe1c79d9cbb2778ad29bc34afd5
David Casey <jacob.casey@dev-jtech.com>
2024-04-13 11:14:22 -0700

    adds a dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ColorModeScript } from '@chakra-ui/react';
+import theme, { ThemeType } from './common/theme.ts';
+
+const initialColorMode = (theme as ThemeType).config.initialColorMode;
 
 const root = document.getElementById('root');
 
@@ -13,6 +17,7 @@ if (!root) {
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={initialColorMode} />
     <App />
   </React.StrictMode>
 );
