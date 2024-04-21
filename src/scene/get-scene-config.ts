606f8849781e7c981193b454c57509388b046917
David Casey <jacob.casey@dev-jtech.com>
2024-04-21 12:43:50 -0700

    updates marker rotation

--- src/scene/get-scene-config.ts
+++ src/scene/get-scene-config.ts
@@ -242,7 +242,7 @@ const getSceneConfig = (): SceneConfig => ({
   cameraParametersUrl: 'assets/camera_para.dat',
   defaultCameraPosition: defaultCameraPosition,
   disableAr: getBoolFromEnv('VITE_DISABLE_AR'),
-  markerXRotation: 0
+  markerXRotation: -90
 });
 
 export default getSceneConfig;
